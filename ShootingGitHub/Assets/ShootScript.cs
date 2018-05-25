7756ff0e4d84e702be8bf7fa30f4d27f33080f5b
DareMeDeveloper <39464881+DareMeDeveloper@users.noreply.github.com>
2018-05-25 17:26:33 -0500

    Gun_Script
    Adding gun and bullit script.

---
+++ ShootingGitHub/Assets/ShootScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float Force;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnMouseDown()
+    {
+        rb.AddForce(0, 0, Force * 100 * Time.deltaTime);
+    }
+}
