f880e2b86aaaeaa982c6f8bb128c0bdec6210ace
DareMeDeveloper <39464881+DareMeDeveloper@users.noreply.github.com>
2018-05-27 12:42:01 -0500

    Add
    Addin Duck and Kill script

--- ShootingGitHub/Assets/ShootScript.cs
+++ ShootingGitHub/Assets/ShootScript.cs
@@ -7,15 +7,22 @@ public class ShootScript : MonoBehaviour {
     public Rigidbody rb;
     public float Force;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Duck")
+        {
+            Debug.Log("I KILLED A DUCK");
+        }
+    }
     void OnMouseDown()
     {
         rb.AddForce(0, 0, Force * 100 * Time.deltaTime);
