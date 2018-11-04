a94e23576d71664482ee49783e9e0e5720f27186
DareMeDeveloper <39464881+DareMeDeveloper@users.noreply.github.com>
2018-11-04 16:52:31 -0600

    Jets Latest Changes
    Jets Latest Changes

---
+++ ShootingGitHub/Assets/CameraControll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rotation = 0;
+
+        if (Input.GetMouseButton(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.DrawLine(ray.origin, hit.point);
+                this.transform.LookAt(hit.point);
+                rotation = Input.mousePosition.x - Screen.width / 2;
+            }
+        }
+
+    }
+}
